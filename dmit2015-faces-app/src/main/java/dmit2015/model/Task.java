c7710cd5786d6704b64acb60c2023ff29d82e455
Sam Wu <swu@nait.ca>
2026-01-26 11:48:50 -0700

    Added example 3 InputText, DataTable, and CommandButton

---
+++ dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
@@ -0,0 +1,14 @@
+package dmit2015.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data   // includes @Getter,@Setter,@ToString,etc
+public class Task {
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 3, max = 255,message = "Description must between {min} and {max} characters")
+    private String description;
+
+}
