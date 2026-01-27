46e658628ea417badfb11cfea2fac8d38e26629b
Sam Wu <swu@nait.ca>
2026-01-27 11:44:25 -0700

    Added to Task class copy constructor, copyOf, and of methods

--- dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
+++ dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
@@ -3,8 +3,11 @@ package dmit2015.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.datafaker.Faker;
 
 @Data   // includes @Getter,@Setter,@ToString,etc
+@NoArgsConstructor
 public class Task {
 
     @NotBlank(message = "Description is required")
@@ -14,5 +17,26 @@ public class Task {
     private TaskPriority priority;
 
     private boolean done;
+
+    // Copy constructor
+    public Task(Task other) {
+       setDescription(other.getDescription());
+       setPriority(other.getPriority());
+       setDone(other.isDone());
+    }
+
+    public static Task copyOf(Task other) {
+        return new Task(other);
+    }
+
+    public static Task of(Faker faker) {
+        Task currentTask = new Task();
+        currentTask.setDescription("Nuke " + faker.fallout().location());
+        currentTask.setPriority(TaskPriority.MEDIUM);
+        currentTask.setDone(faker.bool().bool());
+        return currentTask;
+    }
+
+
 
 }
